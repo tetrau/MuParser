59fe0dfb3f9f3170c9b44cfb7c45f9a16106d146
tetrau <tetrau01@gmail.com>
2018-10-17 17:56:53 +0800

    Improve the MuParser unit test

--- test/test_muanalyst.py
+++ test/test_muanalyst.py
@@ -9,10 +9,12 @@ class TestMuAnalyst(unittest.TestCase):
     class Parser2(MuParser):
         FIXED_DATA = {'b': '//b/text()'}
 
+    SIZE = 100
+
     def setUp(self):
-        self.source_1 = ['<a>{}</a>'.format(i) for i in range(5)]
-        self.source_2 = ['<b>{}</b>'.format(i) for i in range(5)]
-        self.extra_1 = [{'id': i} for i in range(5)]
+        self.source_1 = ['<a>{}</a>'.format(i) for i in range(self.SIZE)]
+        self.source_2 = ['<b>{}</b>'.format(i) for i in range(self.SIZE)]
+        self.extra_1 = [{'id': i} for i in range(self.SIZE)]
 
     def test_mu_analyst(self):
         analyst = MuAnalyst(sources=(self.source_1, self.source_2),
@@ -20,9 +22,5 @@ class TestMuAnalyst(unittest.TestCase):
                             extras=(MuAnalyst.repeat_none(), self.extra_1),
                             processes=2)
         r = [i for i in analyst.analyze()]
-        except_result = [{'a': '0', 'b': '0', 'id': 0},
-                         {'a': '1', 'b': '1', 'id': 1},
-                         {'a': '2', 'b': '2', 'id': 2},
-                         {'a': '3', 'b': '3', 'id': 3},
-                         {'a': '4', 'b': '4', 'id': 4}]
+        except_result = [{'a': str(i), 'b': str(i), 'id': i} for i in range(self.SIZE)]
         self.assertEqual(r, except_result)
